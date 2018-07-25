1cbb5755d903508991a1e046e49ca8c535fbbde4
Ivan_Lamborghini <ivan8lam@gmail.com>
2018-07-25 09:50:49 -0700

    h

--- main.js
+++ main.js
@@ -1,50 +1,98 @@
 
-
-
-
-
-
 var mainState = {
     preload: function(){
-        game.load.image("floor","assets/floor.png");
-		game.load.image("walls","assets/walls.png");
-        game.load.image("player","assets/player.png")
+        game.load.image('floor',"assets/floor.png");
+		game.load.image('walls',"assets/walls.png");
+        game.load.spritesheet('player',"assets/player.png", 68, 68);
+        game.load.spritesheet('enemy',"assets/enemy.png", 40, 40);
+        
     },
     
+    
+    
     create: function(){             
-	
+	    game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.world.enableBody = true;
 	    this.lightAngle = Math.PI/4;
 	    this.numberOfRays = 20;
-	    this.rayLength = 100;
+	    this.rayLength = 70;
         this.floor = game.add.sprite(0,0,"floor");
-		this.wallsBitmap = game.make.bitmapData(640,480);
+		this.wallsBitmap = game.make.bitmapData(1418,803);
 		this.wallsBitmap.draw("walls");
 		this.wallsBitmap.update();
 		game.add.sprite(0,0,this.wallsBitmap);
-		this.player = game.add.sprite(80,80,"player");
+        this.player = game.add.sprite(80 ,80 ,'player');
 		this.player.anchor.setTo(0.5,0.5);
 		this.cursors = game.input.keyboard.createCursorKeys();
 		this.maskGraphics = this.game.add.graphics(0, 0);
 		this.floor.mask=this.maskGraphics
-    },
+		this.floor.mask=this.maskGraphics
+    
+        
+        this.player.frame = 0;
+        this.player.animations.add('down', [0, 1, 2, 3], 10, true);
+        this.player.animations.add('up', [4, 5, 6, 7], 10, true);       
+        this.player.animations.add('right', [8, 9, 10, 11], 10, true);
+        this.player.animations.add('left', [12, 13, 14, 15], 10, true);
+        
+        this.enemies = game.add.group();
+  
+        var rand;
+        var spawnPointCount = 1;
+        //this is an array of objects representing spawn points in the map
+        var spawnPoints = [
+        {x: 1200, y:700},
+        ]
+        
+        for(var i = 0; i < spawnPointCount; i++){
+            rand = Math.floor(Math.random() * spawnPoints.length); //returns number between 0-spawnpoints.length - 1
+            var enemy = game.add.sprite(spawnPoints[rand].x, spawnPoints[rand].y, 'enemy');
+            enemy.frame = 0;
+            enemy.animations.add('down', [0, 1, 2], 10, true);
+            enemy.animations.add('up', [3, 4, 5], 10, true);       
+            enemy.animations.add('right', [6, 7, 8], 10, true);
+            enemy.animations.add('left', [9, 10, 11], 10, true);
+            this.enemies.add(enemy);
+            this.enemyspeed = 10;
+            this.enemies.mask=this.maskGraphics;
+
+        }
+        
+        this.w = game.input.keyboard.addKey(Phaser.Keyboard.W);
+        this.a = game.input.keyboard.addKey(Phaser.Keyboard.A);
+        this.s = game.input.keyboard.addKey(Phaser.Keyboard.S);
+        this.d = game.input.keyboard.addKey(Phaser.Keyboard.D);
+        
+      },
     
     
     update: function(){
+            
+        game.physics.arcade.overlap(this.player, this.enemies, this.restart, null, this);
+        
         
         var xSpeed = 0;
 		var ySpeed = 0;
-		if(this.cursors.up.isDown){
+		if(this.w.isDown){
 			ySpeed -=1;
+            this.player.animations.play('up');
 		}
-		if(this.cursors.down.isDown){
+		else if(this.s.isDown){
 			ySpeed +=1;
+            this.player.animations.play('down');
 		}
-		if(this.cursors.left.isDown){
+		else if(this.a.isDown){
 			xSpeed -=1;
+            this.player.animations.play('left');
 		}
-		if(this.cursors.right.isDown){
+		else if(this.d.isDown){
 			xSpeed +=1;
-		}
+            this.player.animations.play('right');
+        }else{
+            this.player.animations.stop();
+            this.player.frame = 0;
+        }
+        
         
 		if(Math.abs(xSpeed)+Math.abs(ySpeed)<2 && Math.abs(xSpeed)+Math.abs(ySpeed)>0){
 			var color = this.wallsBitmap.getPixel32(this.player.x+xSpeed+this.player.width/2,this.player.y+ySpeed+this.player.height/2);
@@ -81,11 +129,14 @@ var mainState = {
 		}
 		this.maskGraphics.lineTo(this.player.x,this.player.y); 
      	this.maskGraphics.endFill();
-		this.floor.alpha = 0.5+Math.random()*0.5;	
+    },
+    
+    restart: function(){
+    game.state.start('main');    
     }
     
     
 }
-var game = new Phaser.Game(640,480);   
-game.state.add('main', mainState);  
+var game = new Phaser.Game(1418,803);   
+game.state.add('main', mainState); 
 game.state.start('main');
